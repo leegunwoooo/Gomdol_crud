ec7436a02728800bdb9ee53ba90bc09d23bbda39
leegunwoooo <leegunwoo0325@gmail.com>
2024-09-09 23:35:22 +0900

    잘 이해 안되는 거는 담주 화요일까지 주석달아와도 될까요

---
+++ src/main/java/com/example/gwajae/controller/BoardController.java
@@ -0,0 +1,42 @@
+package com.example.gwajae.controller;
+
+import com.example.gwajae.controller.dta.BoardResponse;
+import com.example.gwajae.controller.dta.SaveBoardRequest;
+import com.example.gwajae.controller.dta.UpdateBoardRequest;
+import com.example.gwajae.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class BoardController {
+
+    private final BoardService boardService;
+
+    @PostMapping("/board")
+    public BoardResponse save(@RequestBody SaveBoardRequest request) {
+        return boardService.save(request);
+    }
+
+    @GetMapping("/board-all")
+    public List<BoardResponse> getAll() {
+        return boardService.findAll();
+    }
+
+    @GetMapping("/board/{id}")
+    public BoardResponse findOne(@PathVariable Long id) {
+        return boardService.findOne(id);
+    }
+
+    @PutMapping("/board")
+    public BoardResponse update(@RequestBody UpdateBoardRequest request) {
+        return boardService.update(request);
+    }
+
+    @DeleteMapping("/board/{id}")
+    public void delete(@PathVariable Long id) {
+        boardService.delete(id);
+    }
+}
---
+++ src/main/java/com/example/gwajae/domain/Board.java
@@ -0,0 +1,29 @@
+package com.example.gwajae.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "board_db")
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String content;
+
+    @Builder
+    public Board(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
